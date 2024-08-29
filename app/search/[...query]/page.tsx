cb4b41cda1e50c1fac6d4beda173eb6338feb93e
Shishant Biswas <shishantbiswas@gmail.com>
2024-08-29 19:52:32 +0530

    feat: advance search

--- app/search/[...query]/page.tsx
+++ app/search/[...query]/page.tsx
@@ -1,5 +1,6 @@
 import { AniwatchSearch } from "@/components/aniwatch/aniwatch-search";
-import  SearchSkeleton  from "@/components/fallback-ui/search-skeleton";
+import AnimeSearchFilter from "@/components/aniwatch/aniwatch-search-filter";
+import SearchSkeleton from "@/components/fallback-ui/search-skeleton";
 import TmdbSearch from "@/components/tmdb/tmdb-search";
 import { Suspense } from "react";
 
@@ -13,33 +14,93 @@ export async function generateMetadata({
 
   return {
     title: `'${decodeURIComponent(searchTerm)}' in ${
-      type == "movie" ? "Movie" : type==='anime'? 'Anime' : 'TV Shows'
+      type == "movie" ? "Movie" : type === "anime" ? "Anime" : "TV Shows"
     } - Nextflix`,
   };
 }
 export default async function Query({
   params,
+  searchParams,
 }: {
   params: { query: [string, string] };
+  searchParams?: {
+    type: AnimeType;
+    lang: AnimeLanguage;
+    sort: AnimeSort;
+    status: AnimeStaus;
+  };
 }) {
   const type = params.query[0];
   const searchTerm = params.query[1];
 
   if (type === "anime") {
+    const data: aniwatchSearch = await fetchAniwatchSearch(
+      searchTerm,
+      searchParams?.type,
+      searchParams?.lang,
+      searchParams?.sort,
+      searchParams?.status
+    );
+
+    // console.log(searchParams);
+
     return (
       <div className=" bg-black/80 min-h-screen">
-      <Suspense fallback={<SearchSkeleton />}>
-        <AniwatchSearch searchTerm={searchTerm} />
-      </Suspense>
+        <Suspense fallback={<SearchSkeleton />}>
+          <div className="pb-24 p-4 md:flex-row flex-col flex gap-4">
+            <AnimeSearchFilter search={searchTerm} />
+            <AniwatchSearch data={data} />
+          </div>
+        </Suspense>
       </div>
     );
   }
 
   return (
     <div className=" bg-black/80 min-h-screen">
-    <Suspense fallback={<SearchSkeleton />}>
-      <TmdbSearch search={searchTerm} />
-    </Suspense>
+      <Suspense fallback={<SearchSkeleton />}>
+        <TmdbSearch search={searchTerm} />
+      </Suspense>
     </div>
   );
+}
+
+type AnimeType = "all" | "ona" | "special" | "movie" | "tv" | "ova" | "music";
+type AnimeLanguage = "sub-&-dub" | "sub" | "dub";
+type AnimeStaus =
+  | "finished-airing"
+  | "currently-airing"
+  | "not-yet-aired"
+  | "all";
+type AnimeSort =
+  | "default"
+  | "recently-added"
+  | "name-az"
+  | "most-watched"
+  | "score"
+  | "released-date"
+  | "recently-updated";
+
+async function fetchAniwatchSearch(
+  searchTerm: string,
+  type?: AnimeType,
+  language?: AnimeLanguage,
+  sort?: AnimeSort,
+  status?: AnimeStaus
+) {
+  try {
+    const response = await fetch(
+      `${process.env.ANIWATCH_API}/anime/search?q=${searchTerm}&page=1&type=${
+        type ? type : "all"
+      }$&language=${language ? language : "sub-&-dub"}&sort=${
+        sort ? sort : "default"
+      }&status=${status ? status : "all"}`,
+      { cache: "no-store" }
+    );
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    throw new Error(`Search failed in Search`);
+  }
 }
--- components/aniwatch/aniwatch-search.tsx
+++ components/aniwatch/aniwatch-search.tsx
@@ -1,72 +1,40 @@
-import { SquareArrowOutUpRight } from "lucide-react";
+import { CaptionsIcon, MicIcon, SquareArrowOutUpRight } from "lucide-react";
 import Link from "next/link";
 
-export async function AniwatchSearch({ searchTerm }: { searchTerm: string }) {
-  const data: aniwatchSearch = await fetchAniwatchSearch(searchTerm);
+export async function AniwatchSearch({ data }: { data: aniwatchSearch }) {
 
   return (
     <>
-      <div className="pb-24 p-2">
-        <h1 className=" ml-2 mb-2 text-3xl font-semibold">Search</h1>
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6  gap-3  ">
-          {data.animes.map((e) => (
-            <div
-              key={e.id}
-              className={`relative inline-block text-white rounded-lg overflow-hidden  w-full transition-all duration-200 h-[280px] min-[300px]:h-[320px]  lg:h-[400px] `}
-            >
-              <img
-                className={` size-full object-cover object-center`}
-                src={e.poster}
-                alt={e.name}
-              />
-              <div className="absolute h-full flex items-center justify-between flex-col top-0 left-0 w-full transition-all bg-black/80 backdrop-blur-md opacity-0 hover:opacity-100  p-3 group">
-                <div>
-                  <p
-                    className={` whitespace-normal font-semibold mb-2 leading-[16px] transition-all duration-&lsqb;300&rsqb;  group-hover:leading-[28px] text-xl xl:text-[32px] `}
-                  >
-                    {e.name}
-                  </p>
-                  <p className="text-[13px] md:text-md opacity-10  duration-500 group-hover:opacity-70 ">
-                    {e.type}
-                  </p>
-                  <p
-                    className=" whitespace-normal line-clamp-4  duration-500 group-hover:opacity-70 
-                text-[15px] pt-1 opacity-10"
-                  >
-                    Dub : {e.episodes.dub}
-                  </p>{" "}
-                  <p
-                    className=" whitespace-normal line-clamp-4  duration-500 group-hover:opacity-70 
-                text-[15px] pt-1 opacity-10"
-                  >
-                    Sub : {e.episodes.sub}
-                  </p>
-                </div>
-                <div className=" w-full">
-                  <Link href={`/anime/${e.id}`}>
-                    <button className=" rounded-lg py-1 bg-white/90 flex items-center justify-center gap-2 transition-all  text-black font-semibold w-full">
-                      More Info <SquareArrowOutUpRight size={15} />
-                    </button>
-                  </Link>
-                </div>
-              </div>
-            </div>
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 w-full gap-3  ">
+          {data.animes.map((episode) => (
+             <Link
+             key={episode.id}
+             href={`/anime/${episode.id}`}
+             className="min-w-[150px] w-full lg:w-full h-[300px] rounded-md overflow-hidden group  relative text-end"
+           >
+             <img
+               className="w-full h-full object-cover absolute top-0 group-hover:scale-105 transition-all"
+               src={episode.poster}
+               alt={episode.name}
+             />
+
+             <div className=" absolute bottom-0 left-0 p-2 bg-gradient-to-br from-transparent to-black/80 transition-all group-hover:backdrop-blur-md size-full flex items-end flex-col justify-end capitalize">
+               <h1 className="text-xl font-semibold">{episode.name}</h1>
+               <div className="flex text-sm gap-1">
+                 <p>{episode.type}</p>
+                 <p className="flex items-center gap-1 bg-purple-500/70 rounded-sm  px-1">
+                   <MicIcon size={10} />
+                   {episode.episodes.dub || "NA"}
+                 </p>
+                 <p className="flex items-center gap-1 bg-yellow-500/80 rounded-sm  px-1">
+                   <CaptionsIcon size={10} />
+                   {episode.episodes.sub}
+                 </p>
+               </div>
+             </div>
+           </Link>
           ))}
         </div>
-      </div>
     </>
   );
-}
-
-async function fetchAniwatchSearch(searchTerm: string) {
-  try {
-    const response = await fetch(
-      `${process.env.ANIWATCH_API}/anime/search?q=${searchTerm}&page=1`
-    );
-    const data = await response.json();
-
-    return data;
-  } catch (error) {
-    throw new Error(`Search failed in Search`);
-  }
 }
