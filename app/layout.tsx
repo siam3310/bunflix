bc73358c3fb20466ef6b3522f74bd159748f110c
Shishant Biswas <shishantbiswas@gmail.com>
2024-10-29 02:20:13 +0530

    remove: removed vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,7 @@ import { Inter } from "next/font/google";
 import Navbar from "@/components/navbar";
 import "./globals.css";
 import { Toaster } from "sonner";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import { SearchBarFocusProvider } from "@/context/searchContext";
-import { Analytics } from "@vercel/analytics/react";
 import Providers from "@/context/tanstack-provider";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -21,8 +19,6 @@ export default function RootLayout({
           <body className={inter.className}>
             <Navbar />
             {children}
-            <SpeedInsights />
-            <Analytics />
             <Toaster closeButton richColors />
           </body>
         </SearchBarFocusProvider>
